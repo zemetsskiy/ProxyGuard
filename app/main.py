0d8fb8329db3f7e799a005397f6ce663522a1322
zemetsskiy <zemetsskiy@github.com>
2023-10-30 20:45:52 +0300

    add_proxy post, get methods added

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
-from app.database import db
-from fastapi import FastAPI, Request
+from database import db
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-
+from datetime import datetime
 
 templates = Jinja2Templates(directory="templates")
 
@@ -14,3 +14,28 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.get("/")
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/add_proxy")
+async def add_proxy(request: Request,
+                    customer_name: str = Form(...),
+                    purchase_date: str = Form(...),
+                    duration_months: int = Form(...),
+                    price: float = Form(...),
+                    proxy_package: str = Form(...)):
+    purchase_date = datetime.strptime(purchase_date, '%Y-%m-%d')
+
+    proxy_document = {
+        "customer_name": customer_name,
+        "purchase_date": purchase_date,
+        "duration_months": duration_months,
+        "price": price,
+        "proxy_package": proxy_package
+    }
+    db.proxies.insert_one(proxy_document)
+    return {"message": "Proxy added successfully"}
+
+
+@app.get("/add_proxy")
+async def add_proxy(request: Request):
+    return templates.TemplateResponse("addition.html", {"request": request})
